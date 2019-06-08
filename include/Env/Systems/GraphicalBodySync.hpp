e5989635df43c6b16cf06dbbf5f6a34e4cb9eeaf
sarahkittyy <chrisohlin1@gmail.com>
2019-06-08 17:47:27 -0400

    static scaling methods

--- include/Env/Systems/GraphicalBodySync.hpp
+++ include/Env/Systems/GraphicalBodySync.hpp
@@ -17,11 +17,90 @@ namespace Env::System
 class GraphicalBodySync : public ECSX::System
 {
 public:
+	/**
+	 * @brief Moves all transformable objects to their body position.
+	 * 
+	 */
 	void tick();
+
+	/**
+	 * @brief The scaling from Box2D -> Graphical coordinates.
+	 * 
+	 * @return const float The scale from box2d -> graphical.
+	 */
+	static const float scale();
+
+	/**
+	 * @brief The scaling from Graphical -> Box2D coordinates.
+	 * 
+	 * @return const float Scaling factor from graphical -> box2d.
+	 */
+	static const float invscale();
+
+	/**
+	 * @brief Orients an SFML object such that it's origin is in it's center.
+	 * 
+	 * @tparam T Has setOrigin() and .getLocalBounds().
+	 * @param object The SFML object to orient.
+	 */
+	template <typename T>
+	static void orientCenter(T* object)
+	{
+		//Orient the object on it's center.
+		object->setOrigin(object->getLocalBounds().width / 2.f,
+						  object->getLocalBounds().height / 2.f);
+	}
+
+	/**
+	 * @brief Get the half size of a SFML object.
+	 * 
+	 * @tparam T Has .getLocalBounds()
+	 * @param object The object.
+	 * @return sf::Vector2f The half size of that object.
+	 */
+	template <typename T>
+	static sf::Vector2f halfSize(T& object)
+	{
+		return sf::Vector2f(
+			object.getLocalBounds().width / 2.f,
+			object.getLocalBounds().height / 2.f);
+	}
+
+	/**
+	 * @brief Get the half size of a float rect.
+	 * 
+	 * @param object The rect.
+	 * @return sf::Vector2f The rect's half size.
+	 */
+	template <>
+	sf::Vector2f halfSize(sf::FloatRect& object)
+	{
+		return sf::Vector2f(
+			object.width / 2.f,
+			object.height / 2.f);
+	}
+
+	/**
+	 * @brief Convert an sf::Vector2f to a b2Vec2
+	 * 
+	 * @param in The input vector.
+	 * @param scaleCoords Whether or not to scale the vector as well.
+	 * @return b2Vec2 The converted vector.
+	 */
+	static b2Vec2 fromVector(sf::Vector2f in, bool scaleCoords = true);
+
+	/**
+	 * @brief Convert a b2Vec2 to an sf::Vector2f
+	 * 
+	 * @param in The input vector.
+	 * @param scaleCoords Whether or not to scale the vector coords as well.
+	 * @return sf::Vector2f The output vector.
+	 */
+	static sf::Vector2f toVector(b2Vec2 in, bool scaleCoords = true);
 
 private:
 	/// Transform from Box2D to graphical.
-	const float SCALE = 20.f;
+	static const float SCALE;
 };
 
 }
--- src/Env/Systems/GraphicalBodySync.cpp
+++ src/Env/Systems/GraphicalBodySync.cpp
@@ -6,14 +6,49 @@ namespace Env::System
 void GraphicalBodySync::tick()
 {
 	each<Env::Component::Body, Env::Component::Transformable>(
-		[this](ECSX::Entity* e) {
+		[](ECSX::Entity* e) {
+			//Grab the two components
 			Env::Component::Body* body				 = e->get<Env::Component::Body>();
 			Env::Component::Transformable* transform = e->get<Env::Component::Transformable>();
 
-			b2Vec2 body_pos = body->body->GetPosition();
-			body_pos *= SCALE;
-			transform->setPosition(body_pos.x, body_pos.y);
+			// Scale the body position
+			sf::Vector2f graphical_pos = toVector(body->body->GetPosition());
+			transform->setPosition(graphical_pos);
 		});
+}
+
+////////////////////////////
+//
+// Graphical Scaling Methods
+//
+////////////////////////////
+
+const float GraphicalBodySync::SCALE = 20.f;
+
+const float GraphicalBodySync::scale()
+{
+	return SCALE;
+}
+
+const float GraphicalBodySync::invscale()
+{
+	return 1.f / SCALE;
+}
+
+b2Vec2 GraphicalBodySync::fromVector(sf::Vector2f in, bool scaleCoords)
+{
+	b2Vec2 ret;   // return vector
+	ret.x = in.x * (scaleCoords ? invscale() : 1);
+	ret.y = in.y * (scaleCoords ? invscale() : 1);
+	return ret;
+}
+
+sf::Vector2f GraphicalBodySync::toVector(b2Vec2 in, bool scaleCoords)
+{
+	sf::Vector2f v;   //return vector.
+	v.x = in.x * (scaleCoords ? scale() : 1);
+	v.y = in.y * (scaleCoords ? scale() : 1);
+	return v;
 }
 
 }
